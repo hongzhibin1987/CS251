039b4a401a635d3b41f120632c6750574d002481
Zhibin "Bing" Hong <hongzhibin1987@gmail.com>
2021-04-13 09:12:00 -0600

    4/13

--- Lab05/src/MedianList.java
+++ Lab05/src/MedianList.java
@@ -1,38 +1,153 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class MedianList {
-    public void main(String[] args) {
+class MedianList
+{
+    ArrayList<Integer> medianList;
+    int median;
 
-    }
+    //push value in array
+    void push(int number)
+    {
+        if(medianList.size() == 0)
+        {
+            System.out.println("Type the number you want to push in");
+            medianList.add(number);
+            System.out.println("After push:");
+            System.out.println(medianList);
+            return;
+        }
+        else
+        {
+            System.out.println("Before push:");
+            System.out.println(medianList);
+            // add number into the median list.
+            medianList.add(number);
+            try {
+                // determine the value of number input and median number.
+                assert(number > median);
+                System.out.println("The number you entered is larger than the median");
 
-    public boolean isMedian(int x, ArrayList<Integer> values){
-        int bigX = 0;
-        int smallX = 0;
-        int valueSize = values.size();
+                // create a temp array and copy the old array into the temp one.
+                Object[] temp=medianList.toArray();
+                int[] array = new int[temp.length];
+                for(int i=0;i<temp.length;i++)
+                {
+                    array[i]=(int) temp[i];
+                }
+                Arrays.sort(array);
+                for(int i=0;i<array.length;i++)
+                {
+                    if(i==array.length/2)
+                    {
+                        median=array[i];
+                    }
+                }
 
-        for (int i = 0; i< valueSize; i++){
-            if (values.indexOf(i) > x) {
-                bigX ++;
+            } catch (Exception e) {
+                System.out.println("The number you entered is smaller or equal to the median one");
+                Object[] temp=medianList.toArray();
+                int[] array = new int[temp.length];
+                for(int i=0;i<temp.length;i++)
+                {
+                    array[i]=(int) temp[i];
+                }
+                Arrays.sort(array);
+                for(int i=0;i<array.length;i++)
+                {
+                    if(i==array.length/2)
+                    {
+                        median=array[i];
+                    }
+                }
             }
-            else if (values.indexOf(i)<x) {
-                smallX ==;
+            System.out.println("After push the new number:");
+            System.out.println(medianList);
+        }
+    }
+
+    //pop out the median value from array
+    int pop()
+    {
+        try {
+            int index=medianList.indexOf(median);
+            int disposalNumber;
+            // get the index of the median and list the index
+            System.out.println("The index of the  Median list is "+index);
+            System.out.println("Before pop the array is:");
+            System.out.println(medianList);
+
+            // pop out the median
+            disposalNumber = medianList.remove(medianList.indexOf(median));
+            Object[] temp=medianList.toArray();
+            int[] array = new int[temp.length];
+            for(int i=0;i<temp.length;i++)
+            {
+                array[i]=(int) temp[i];
+            }
+            Arrays.sort(array);
+            for(int i=0;i<array.length;i++)
+            {
+                if(i==array.length/2)
+                {
+                    median=array[i];
+                }
+            }
+            System.out.println("After pop the array is :");
+            System.out.println(medianList);
+            return disposalNumber;
+
+        } catch (Exception e) {
+            System.out.println("The array is empty.");
+        }
+        return 0;
+
+    }
+
+    //find median
+    int peek()
+    {
+        return median;
+    }
+
+    //array to string
+    public String toString ()
+    {
+        return  medianList.toString();
+    }
+
+    public  MedianList() {
+        // get the user input of array
+        medianList=new ArrayList<Integer>();
+        System.out.println("Enter the array:");
+        Scanner input= new Scanner(System.in);
+        String line=input.nextLine();
+        Scanner arrayinput = new Scanner(line);
+
+        while (arrayinput.hasNextInt())
+        {
+            medianList.add(arrayinput.nextInt());
+        }
+        System.out.println("The following added:");
+
+        System.out.print(medianList);
+
+        // create temp array for future list
+        Object[] temp=medianList.toArray();
+        int[] array = new int[temp.length];
+        for(int i=0;i<temp.length;i++)
+        {
+            array[i]=(int) temp[i];
+        }
+        Arrays.sort(array);
+        for(int i=0;i<array.length;i++)
+        {
+            if(i==array.length/2)
+            {
+                median=array[i];
             }
         }
-
-        if(bigX > valueSize/2) {
-            return false;
-        }
-        else if (smallX > valueSize/2) {
-            return false;
-        }
-        return true;
-    }
-
-    public boolean testCorrectMedian(){
-
-    }
-
-    public boolean testCorrectString(){
-
+        System.out.println("The median is "+median);
     }
 }
--- Lab05/src/MedianListTest.java
+++ Lab05/src/MedianListTest.java
@@ -1,49 +1,116 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MedianListTest {
-    ArrayList<Integer> values = new ArrayList<Integer>();
 
-    void MedianList() {
+    static MedianList testList;
 
-    }
+    // determine whether a number is median or not.
+    static boolean isMedian(int x)
+    {
+        Object[] temp= testList.medianList.toArray();
+        int[] array = new int[temp.length];
 
-    public void push(int x) {
-        values.add(x);
-    }
-
-    public int pop() {
-        int popValue = peek();
-        values.remove(popValue);
-        return popValue;
-    }
-
-    public int peek() {
-        int nextValueLocation;
-        int nextValue;
-        if (values.size() % 2 == 0) {
-            nextValueLocation = (values.size() / 2);
-        } else {
-            nextValueLocation = values.size() / 2 + 1;
+        for(int i=0;i<temp.length;i++)
+        {
+            array[i]=(int) temp[i];
         }
-        nextValue = values.indexOf(nextValueLocation);
-        return nextValue;
-    }
 
-    public boolean isEmpty() {
-        return values.isEmpty();
-    }
-
-    public String toString() {
-        int n = values.size();
-        if (n == 0) {
-            return "[]";
-            StringBuilder sb = new StringBuilder();
-            for (int i = values.size() - 1; i == 0; i--) {
-                int num = values.get(i);
-                sb.append(num);
+        Arrays.sort(array);
+        for(int i=0;i<array.length;i++)
+        {
+            if(i==array.length/2)
+            {
+                if(x==array[i])
+                {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
-            String result = sb.toString();
-            return result;
         }
+        return false;
+    }
+
+    // main body of the logic to test the whether the median is correct.
+    static boolean testCorrectMedian()
+    {
+        int selectNumber;
+        int pushNumber;
+        int returnNumber;
+        boolean returnBoolean;
+
+        while (true)
+        {
+            // print out main program
+            Scanner select=new Scanner(System.in);
+            System.out.println("Select an option:");
+            System.out.println("1.push a new number");
+            System.out.println("2.pop the median");
+            System.out.println("3.peek");
+            System.out.println("4.validate");
+            System.out.println();
+            selectNumber=select.nextInt();
+            switch (selectNumber) {
+                case 1:
+                    System.out.println("input the push number");
+                    // get the number from user input
+                    pushNumber=select.nextInt();
+                    // push the number into the arraylist
+                    testList.push(pushNumber);
+                    break;
+                case 2:
+                    // popout the median number
+                    returnNumber=testList.pop();
+                    break;
+                case 3:
+                    // review the next number to be popped
+                    returnNumber=testList.peek();
+                    if (isMedian(returnNumber))
+                    {
+                        System.out.println("The test median correct!");
+                    }
+                    else {
+                        System.out.println("The test median incorrect!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Validate & Exit? (type true/false)");
+                    //  makes sure the median revealed by peek is correct after a series of pushes and pops.
+                    returnBoolean=select.nextBoolean();
+                    if (returnBoolean)
+                    {
+                        System.out.println("Validation in progress");
+                        return true;
+                    }
+                    else {
+                        System.out.println("Resume previous procedure");
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    //makes sure the String returned from toString is correct after a series of pushes and pops.
+    static boolean testCorrectString()
+    {
+        if(testList.toString().equals(testList.medianList.toString()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main (String args[]) {
+        testList=new MedianList();
+        System.out.println("Media validation is "+testCorrectMedian());
+        System.out.println("The String output is "+testCorrectString());
     }
 }
-
