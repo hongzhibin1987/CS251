51d596fa440349b6ca64c815779986b054ec6c33
Zhibin "Bing" Hong <hongzhibin1987@gmail.com>
2021-04-15 23:09:11 -0600

    4/15 bug fix.
    Now should be able to display updated sql via setResult

--- Lab06new/src/EmployeeDB.java
+++ Lab06new/src/EmployeeDB.java
@@ -2,14 +2,22 @@ import java.sql.*;
 import java.util.Random;
 import java.util.Scanner;
 
-
 /*
-a) Add employees to the employee table.
-b) Add payroll information to the appropriate table for each new employee.For example, for a salaried employee add the payroll information to the *salariedEmployees* table.
-g) Validate the formatting of the SSN and the birthday and make the user re-enter this information if they mess up.
-// create a DBmanager class to manage all connection, disconnections, and queries.
-// option B: model class/object- all fields we needs that
-// future UML and flowchart to better guide the mindflow
+DB Installation: Derby side
+1. Install Derby. Do not install under C/program files/java; install it anywehre else you have permission;
+2. once done, change your environment with windows-env and change working environment accordingly. locate both %DERBY_HOME% and %JAVA_HOME% accordingly;
+3. Start Derby service by clicking startNetworkServer.bat;
+4. get the opening port properly;
+5. create a new db in derby console;
+6. in derby db, execute the employees.sql, and then go to derby folder /bin, you should be able to see the folder in file explorer.
+7. IMPORTANT: everytime when you have to use intellij, make sure the startNetworkServer.bat is open. otherwise Intellij won't have access on java.
+
+DB link: IntelliJ
+1. get the plugin named "DB navigator"
+2. make sure derby server is open and running. when configuring DB connection, do not use absolute path but localhost: XXXX (4 digit of your port)/dbname;
+3. make sure choose derbyclient as your driver
+4. in Intellij, go file-project structure-library, add both derby.jar and derbyclient.jar into the libraby of the project.
+
  */
 public class EmployeeDB {
 
@@ -22,7 +30,7 @@ public class EmployeeDB {
         String userInputDepartmentName = null;
         String userInputEmploymentType = null;
         double userInputBaseSalary = 0.00;
-        double userInputBonus =0.00;
+        double userInputBonus = 0.00;
         double userInputCommissionRate = 0.00;
         double userInputGrossSales = 0.00;
         double userInputHours = 0.00;
@@ -30,7 +38,7 @@ public class EmployeeDB {
         double userInputWeeklySalary = 0.00;
         PreparedStatement insertNewPerson;
 
-        if (userEmploymentTypeInput ==1){
+        if (userEmploymentTypeInput == 1) {
             System.out.println("We will ask you some basic questions of the Base+Commission Employee");
 
             // get user input of the Employee class. general questions.
@@ -66,7 +74,7 @@ public class EmployeeDB {
             userInputBonus = BE.nextDouble();
 
             // create new object of BE.
-            BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee(userInputFirstName, userInputLastName, userInputSSN, userInputBD, userInputEmploymentType, userInputDepartmentName,userInputGrossSales,userInputCommissionRate,userInputBaseSalary, userInputBonus);
+            BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee(userInputFirstName, userInputLastName, userInputSSN, userInputBD, userInputEmploymentType, userInputDepartmentName, userInputGrossSales, userInputCommissionRate, userInputBaseSalary, userInputBonus);
             // test print the whole thing
             //System.out.printf("%s%n%s: $%,.2f%n%n", basePlusCommissionEmployee, "earned", basePlusCommissionEmployee.earnings());
             //System.out.println(userInputDepartmentName);
@@ -106,22 +114,19 @@ public class EmployeeDB {
             insertNewPerson.executeUpdate();
 
             insertNewPerson.close();
-        }
-        else if (userEmploymentTypeInput ==2){
+        } else if (userEmploymentTypeInput == 2) {
             System.out.println("We will ask you some basic questions of the Commission Employee");
             // get user input of the Employee class. general questions.
             // get user input for BC specific questions.
             // create new object of CE.
             // send the data into sql.
-        }
-        else if (userEmploymentTypeInput ==3){
+        } else if (userEmploymentTypeInput == 3) {
             System.out.println("We will ask you some basic questions of the Hourly Employee");
             // get user input of the Employee class. general questions.
             // get user input for BC specific questions.
             // create new object of HE.
             // send the data into sql.
-        }
-        else if (userEmploymentTypeInput ==4){
+        } else if (userEmploymentTypeInput == 4) {
             System.out.println("We will ask you some basic questions of the Salaried Employee");
             // get user input of the Employee class. general questions.
             // get user input for BC specific questions.
@@ -130,7 +135,7 @@ public class EmployeeDB {
         }
     }
 
-    public static void removeEmployee(Statement statement, Connection connection, String DATABASE_URL) throws SQLException{
+    public static void removeEmployee(Statement statement, Connection connection, String DATABASE_URL) throws SQLException {
         String userDeleteSSN;
         PreparedStatement deleteEmployee;
 
@@ -170,15 +175,15 @@ public class EmployeeDB {
         deleteEmployee.close();
 
 
-
-        System.out.print ("The staff with SSN of " + userDeleteSSN + " has been deleted.\n");
+        System.out.print("The staff with SSN of " + userDeleteSSN + " has been deleted.\n");
     }
 
-    public static void ssnValidation(){}
+    public static void ssnValidation() {
+    }
 
     public static void main(String args[]) throws SQLException, ClassNotFoundException {
 
-       // employees = new ArrayList();
+        // employees = new ArrayList();
 
         final String DATABASE_URL = "jdbc:derby://localhost:1527/test;create=true";
 
@@ -189,21 +194,19 @@ public class EmployeeDB {
                 "LEFT JOIN COMMISSIONEMPLOYEES on EMPLOYEES.socialsecurityNumber = COMMISSIONEMPLOYEES.SOCIALSECURITYNUMBER\n" +
                 "LEFT JOIN HOURLYEMPLOYEES on EMPLOYEES.socialsecurityNumber = HOURLYEMPLOYEES.SOCIALSECURITYNUMBER\n" +
                 "LEFT JOIN SALARIEDEMPLOYEES on EMPLOYEES.socialsecurityNumber = SALARIEDEMPLOYEES.SOCIALSECURITYNUMBER\n";
-                //"ORDER BY EMPLOYEES.SOCIALSECURITYNUMBER";
+        //"ORDER BY EMPLOYEES.SOCIALSECURITYNUMBER";
 
         // use try-with-resource to connect and query the database
         try (
-            Connection connection = DriverManager.getConnection(DATABASE_URL);
-            PreparedStatement statement = connection.prepareStatement(SELECT_QUERY, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            ResultSet resultSet = statement.executeQuery()) {
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int numberOfColumns = metaData.getColumnCount();
-
-            int userInput=0;
+                Connection connection = DriverManager.getConnection(DATABASE_URL))
+        {
+            int userInput = 0;
             int userEmploymentTypeInput;
-
-            while (userInput != 4){
-
+            while (userInput != 4) {
+                PreparedStatement statement = connection.prepareStatement(SELECT_QUERY, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                ResultSet resultSet = statement.executeQuery();
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int numberOfColumns = metaData.getColumnCount();
                 resultSet.beforeFirst();
                 System.out.printf("***WELCOME TO EMPLOYEE PAYROLL MANAGEMENT SYSTEM***\n");
                 // print out main program body with loop and options
@@ -238,12 +241,11 @@ public class EmployeeDB {
                     // main body of using different "addEmployee"
                     case 2:
                         System.out.println("Please enter type of the employment. Please enter \n1 for BE: based+commission \n2 for CE: commission \n3 for HE: hourly \n4 for SE: salaried \n5 return to previous menu");
-                        userEmploymentTypeInput =  select.nextInt();
+                        userEmploymentTypeInput = select.nextInt();
 
-                        if (userEmploymentTypeInput == 5){
+                        if (userEmploymentTypeInput == 5) {
                             break;
-                        }
-                        else if(userEmploymentTypeInput <5) {
+                        } else if (userEmploymentTypeInput < 5) {
                             addEmployee(userEmploymentTypeInput, statement, connection, DATABASE_URL);
                         }
                         break;
@@ -254,18 +256,19 @@ public class EmployeeDB {
                     case 4:
                         System.out.println("Thank you for using the program. Bye!");
                         break;
-                        // the following statement takes care the validation
+                    // the following statement takes care the validation
                     default:
                         System.out.println("Option not available!");
                         break;
                 }
             }
         }
-        catch (SQLException e) {
-            e.printStackTrace();
+        catch(SQLException e){
+                e.printStackTrace();
+            }
         }
     }
-}
+
 
 /*
 public class PayrollSystemTest {
